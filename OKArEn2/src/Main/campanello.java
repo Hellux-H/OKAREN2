cabb3dc65077238d1bdd46b6631e86838e507ce5
alexandru.vucec <alexandru.vucec@PC-2019.itslifetech.lan>
2025-06-17 12:52:33 +0200

    commento

---
+++ OKArEn2/src/Main/campanello.java
@@ -0,0 +1,8 @@
+package Main;
+
+public class campanello {
+		public static void main(String[] args) {
+		System.out.println("Hello, Campanello!");
+		System.out.println("This is a simple Java program.");
+	}
+}
